aa907e78dec873d0df5775ad3c344f722e0774fc
Jason-Olson <contact.j.olson@gmail.com>
2017-06-05 19:18:50 -0600

    training att working

--- weight_panel.py
+++ weight_panel.py
@@ -13,7 +13,7 @@ def get_panel_attendance():
     names_list = df.iloc[:prior_month_start_idx,3].as_matrix()
     names_used_list = remove_unused(names_list,df)
     df2 = make_attendance_df(names_used_list,df,prior_month_start_idx)
-    return df2
+    return df2, names_used_list
 
 def remove_unused(names_list,df):
     mask = []
@@ -52,5 +52,47 @@ def make_attendance_df(names_used_list,df,prior_month_start_idx):
     df_new = pd.DataFrame(stacked,columns=cols)
     return df_new
 
+def get_training_attendance(df,names_used_list):
+    df2 = pd.read_excel('data/training_att.xlsx')
+    # t = pd.tslib.Timestamp.now()
+    # curr_Month = t.month
+    # curr_Year = t.year
+    ## for testing since data not current
+    curr_Month = 2
+    curr_Year = 2017
+    dfa = df2.loc[df2['Attendance Type ']=='Expert Taste Panel Training']
+    curr_idx = np.where((dfa['Date'].map(lambda x: (x.month)==2)) & (dfa['Date'].map(lambda x: (x.year)==2017)))[0][0]
+    curr_num = _get_curr_num(dfa,curr_Month,curr_Year)
+    print(curr_num)
+
+    df_curr = dfa.iloc[curr_idx:,:]
+    curr_att_per = np.empty_like(names_used_list)
+    for x,name in enumerate(names_used_list):
+        idx_list = np.where(df_curr['Name']==name)[0]
+        curr_att_per[x] = idx_list.shape[0] / float(curr_num)
+
+    prior_num = dfa['Date'].nunique() - curr_num
+    df_prior = dfa.iloc[:curr_idx,:]
+    prior_att_per = np.empty_like(names_used_list)
+    for x,name in enumerate(names_used_list):
+        idx_list = np.where(df_prior['Name']==name)[0]
+        prior_att_per[x] = idx_list.shape[0] / float(prior_num)
+
+    col_list = list(df.columns)
+    col_list.extend(('curr_train_att','prior_train_att'))
+    X = df.values
+    stacked = np.column_stack((X,curr_att_per,prior_att_per))
+    df_new = pd.DataFrame(stacked,columns=col_list)
+    return df_new
+
+def _get_curr_num(dfa,curr_Month,curr_Year):
+    d = dfa['Date'].unique()
+    d = pd.DataFrame(d)
+    num = d.loc[(d[0].map(lambda x: x.month)==curr_Month) & (d[0].map(lambda x: x.year)==curr_Year)].shape[0]
+    return num
+
+
+
 if __name__ == '__main__':
-    df = get_panel_attendance()
+    df,names_used_list = get_panel_attendance()
+    df = get_training_attendance(df,names_used_list)
