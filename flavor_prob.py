6862d0cefd051dd05d443d996150733c46f8603e
Jason-Olson <contact.j.olson@gmail.com>
2017-06-11 03:02:54 -0600

    adds comment translation column

--- flavor_prob.py
+++ flavor_prob.py
@@ -18,6 +18,8 @@ def create_flavor_dictionary():
             if type(x[y])==str:
                 lem_word = lemmatize_words(nlp,x[y])
                 flav_dict[x[0]].append(lem_word)
+    with open('data/flav_dict.pickle', 'wb') as handle:
+        pickle.dump(flav_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return flav_dict
 
 def lemmatize_words(nlp,words):
@@ -28,5 +30,52 @@ def lemmatize_words(nlp,words):
     lem_word = " ".join(tok_list)
     return lem_word
 
+def flav_dict_from_pickle():
+    with open('data/flav_dict.pickle', 'rb') as handle:
+        flav_dict = pickle.load(handle)
+    return flav_dict
+
+def score_flavor(df,flav_dict):
+    X = df[['cClarity', 'cAroma', 'cFlavor', 'cMouthfeel', 'cFresh',
+            'cNotFresh']].values
+    nlp = spacy.load('en')
+
+    out_list = []
+    for i in range(X.shape[0]):
+        #Leave th Fresh comments out of this
+        text = str(X[i,0]) + str(X[i,1]) + str(X[i,2]) + str(X[i,3]) + str(X[i,5])
+        lemm_text = lemmatize_words(nlp,text)
+        score_dict={}
+        #5for matching key. 2.5 for matching value.
+        for key in flav_dict:
+            score = 0
+            ix = 0
+            while text.find(key,ix)!= -1:
+                score +=5
+                ix = text.find(key,ix) + 1
+            for val in flav_dict[key]:
+                ix = 0
+                while text.find(val,ix)!= -1:
+                    score +=2.5
+                    ix = text.find(val,ix) + 1
+            score_dict[key] = score
+        out_list.append(score_dict)
+    # get percentage chance for each individual
+    for i in range(len(out_list)):
+        used_dict={k:v for k,v in out_list[i].items() if v > 0}
+        if len(used_dict)!=0:
+            sum_of_dict = sum(out_list[i].values())
+            p_used_dict = {k:v/float(sum_of_dict) for k,v in used_dict.items()}
+            out_list[i] =p_used_dict
+        else:
+            out_list[i] = {'could not translate comments':1}
+
+    df['flav_dict']= out_list
+    return df
+
 if __name__ == '__main__':
-    flav_dict = create_flavor_dictionary()
+    # flav_dict = create_flavor_dictionary()
+    flav_dict = flav_dict_from_pickle()
+    df = pd.read_pickle('data/joined_results.pickle')
+    df = df[df['correct']==1]
+    df = score_flavor(df,flav_dict)
